2a500c9ed2abd9ba82627fe47d32e84f63680397
Guojun Chen <guojun.chen@yale.edu>
2025-04-03 17:24:59 -0400

    update

--- examples/stm32-lock.py
+++ examples/stm32-lock.py
@@ -1,16 +1,20 @@
 import json
-from podtp import Podtp, print_t
-import time
+from podtp import Podtp
+import argparse
 
-def main():
+def main(lock: bool):
     with open('config.json', 'r') as file:
         config = json.loads(file.read())
     
     podtp = Podtp(config)
     if podtp.connect():
-        podtp.send_ctrl_lock(True)
+        if podtp.send_ctrl_lock(lock):
+            print(('Lock' if lock else 'Unlock') + ' [OK]')
         podtp.disconnect()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Lock or unlock the drone control.')
+    parser.add_argument('--unlock', action='store_false', help='Unlock the drone control')
+    args = parser.parse_args()
+    main(args.unlock)
